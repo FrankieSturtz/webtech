deab74f9fc8328724bd7a7775b6e4f232fc4ae98
FrankieSturtz <fasturtz@ualr.edu>
2025-12-08 22:16:17 +0000

    Mars Rover Final 2

--- mars-rovers-final/assets/css/img/js/script.js
+++ mars-rovers-final/assets/css/img/js/script.js
@@ -1,12 +1,27 @@
-$(document).ready(function() {
-
-   $('.fade').slick({
-  dots: true,
-  infinite: true,
-  speed: 500,
-  fade: true,
-  cssEase: 'linear'
-});
-
-
+$('.fade').slick({
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        dots: true,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        dots: true,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
 });
